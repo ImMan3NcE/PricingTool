2f0355558e317e5eb03095aceb547afc5532815a
ImManeNcE <imman3nce@gmail.com>
2023-08-16 17:25:55 +0200

    Ustalono minimalna wiekość okna

--- PricingTool/App.xaml.cs
+++ PricingTool/App.xaml.cs
@@ -11,4 +11,25 @@ public partial class App : Application
 
         MainPage = new NavigationPage(new ProjectDataView());
     }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        const int newWidth = 1400;
+        const int newHeight = 700;
+
+
+        window.X = 500;    
+        window.Y = 200;    
+
+        window.Width = newWidth;
+        window.Height = newHeight;
+
+        window.MinimumHeight = newHeight;
+        window.MinimumWidth = newWidth;
+
+        return window;
+
+    }
 }
